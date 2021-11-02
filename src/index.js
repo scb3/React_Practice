0922a2e18da1a94a416bdc9594dd8bab984aa8c8
scb3 <scott.c.brooks@outlook.com>
2021-11-02 12:28:28 +0000

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./components/App";
+
+ReactDOM.render(<App />, document.getElementById("root"));
+
+//so the Card component can be reused for other contacts.
+//4. Import the contacts.js file to create card components.
